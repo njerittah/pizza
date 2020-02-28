4641247b87383f6d8d6cb0feb1fdfa9b39e47ee4
njerittah <vivianbianca92@gmail.com>
2020-02-28 13:34:09 +0300

    added crust function

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,26 @@ function Size(xl,large,medium, small){
   this.medium= medium
   this.small= small
 }
-function Toppings=(pepperoni, mushrooms, onions, sausage, bacon, black olives, green peppers)
+
+function Crust(thin,flatbread,focaccia,custom,glutenfree){
+  this.thin="thin"
+  this.flatbread="flatbread"
+  this.focaccia="focaccia"
+  this.custom="custom"
+  this.glutenfree= "glutenfree"
+}
+
+
+
+
+<option>Thin</option>
+<option>Flat Bread</option>
+<option>Thick</option>
+<option>Focaccia</option>
+<option>Authentic WoodFried</option>
+<option>Custom</option>
+<option>Gluten Free</option>
+function Toppings(pepperoni, mushrooms, onions, sausage, bacon, black olives, green peppers){
 this.pepperoni= pepperoni
 this.mushrooms= mushrooms
 this.onions= onions
@@ -17,3 +36,4 @@ this.sausage= sausage
 this.bacon = bacon
 this.blackolives= black olives
 this.greenpeppers= green peppers
+}
