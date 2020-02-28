58dacef3408e5174dfdf773f2ab2d5c1ce08c707
njerittah <vivianbianca92@gmail.com>
2020-02-28 13:28:44 +0300

    added toppings functions

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,11 @@ function Size(xl,large,medium, small){
   this.medium= medium
   this.small= small
 }
+function Toppings=(pepperoni, mushrooms, onions, sausage, bacon, black olives, green peppers)
+this.pepperoni= pepperoni
+this.mushrooms= mushrooms
+this.onions= onions
+this.sausage= sausage
+this.bacon = bacon
+this.blackolives= black olives
+this.greenpeppers= green peppers
