05bf002f338d68407434a3b26430b1848f22b939
njerittah <vivianbianca92@gmail.com>
2020-03-01 22:31:34 +0300

    add changes to js and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,71 @@
-
-
-
-
-
-
-
-function Pizza(size,crust,Toppings){
-  this.size = size;
-  this.crust = crust;
-  this.toppings = toppings;
+var totalCosts = [];
+function order(size, crust, Topping,){
+    this.size = size;
+    this.crust = crust;
+    this.Topping = Topping;
+    this.price = 0;
 }
-
-function Size(xl=2000,large=1500,medium=1000, small=500){
-  this.xl=xl;
-  this.large= large;
-  this.medium= medium;
-  this.small= small;
+var pizzaSize = ["Small", "Medium", "Large"];
+var pizzaCrust = ["Crispy", "Stuffed", "Gluten-free"];
+var pizzaTopping = ["Peperonni", "Sausage", "Chicken",];
+order.prototype.cost = function(){
+    if (this.size === pizzaSize[0]){
+        this.price += 300;
+    }
+    else if (this.size === pizzaSize[1]){
+        this.price += 500;
+    }
+    else if (this.size === pizzaSize[2]){
+        this.price += 600;
+    }
+    if (this.crust === pizzaCrust[0]){
+        this.price += 100;
+    }
+    else if (this.crust === pizzaCrust[1]){
+        this.price += 130;
+    }
+    else if (this.crust === pizzaCrust[2]){
+        this.price += 150;
+    }
+    if (this.Topping === pizzaTopping[0]){
+        this.price += 100;
+    }
+    else if (this.Topping === pizzaTopping[1]){
+        this.price += 100;
+    }
+    else if (this.Topping === pizzaTopping[2]){
+        this.price += 100;
+    }
+    return this.price;
 }
-
-function Crust(thin=100,flatbread=200,focaccia=200,custom=150,glutenfree=300){
-  this.thin="thin"
-  this.flatbread="flatbread"
-  this.focaccia="focaccia"
-  this.custom="custom"
-  this.glutenfree= "glutenfree"
+order.prototype.totalCost = function(){
+    var orderTotal = 0;
+    for(var order =0; order < totalCosts.length; order++){
+        orderTotal += totalCosts[order];
+    }
+    return orderTotal;
 }
-
-function Toppings(pepperoni, mushrooms, onions, sausage, bacon, blackolives, greenpeppers){
-this.pepperoni= pepperoni
-this.mushrooms= mushrooms
-this.onions= onions
-this.sausage= sausage
-this.bacon = bacon
-this.blackolives= blackolives
-this.greenpeppers= greenpeppers
-}
-
-
-$(document).ready(function(){
-
-  $("form").submit(function() {
-var size=$("#siz").val()
-var crus = $("#crus").val()
-var toppings= $("#toppin").val()
-var number=$("#num").val()
-var deliver=$("#deliv").val()
-
-
-var =$("#numb").val()
-var delive = $("#deliv").val()
-)};
+$(document).ready(function () {
+    $("input#totalCost").click(function (event) {
+        event.preventDefault();
+        var sizes = $("select#size").val();
+        var crusts = $("select#crust").val();
+        var Toppings = $("select#topping").val();
+        var newPizzaOrder = new order(sizes, crusts, Toppings);
+        newPizzaOrder.cost();
+        totalCosts.push(newPizzaOrder.price);
+        $("#tc").text(newPizzaOrder.totalCost());
+    });
+});
+$(document).ready(function () {
+  $("#totalCost").click(function () {
+      alert("Thank you for Your Purchase!")
+  });
+});
+$(document).ready(function () {
+    $("#delivery").click(function () {
+        prompt("Name")
+        prompt("Location")
+        alert("Thank you for Your Purchase!Your order will be delivered to your location")
+    });
+});
