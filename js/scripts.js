d9433808c753412653f7c0ee2441a6fde1c1284a
njerittah <vivianbianca92@gmail.com>
2020-03-04 17:54:45 +0300

    new

--- js/scripts.js
+++ js/scripts.js
@@ -18,19 +18,12 @@ var crust = $('input[name=crust]:checked').attr('value');
 var toppings=$('input[name=Topping]:checked').attr('value');
 var delivery=$('input:radio[name=delivery]:checked').attr('value');
 var number = $('#number').val();
-// console.log(number);
-// console.log(size);
-// console.log(crust);
-// console.log(toppings);
 var order = new Order(size, crust, toppings, number);
-// console.log(order.total());
+alert(order.total());
 
-// console.log(order.add());
-// if (delivery ===1){
-//   alert(order.Order());
-//   alert('your bill is'+ order.total());
-document.getElementById("tt").innerHTML = new Order();
-document.getElementById("mm").innerHTML= order.total()
+document.getElementById("mm").innerHTML = order.total();
 
 });
 });
+
+
